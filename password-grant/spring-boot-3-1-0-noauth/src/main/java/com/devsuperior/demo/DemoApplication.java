9480945971e9731cd9f3621677662b99f8e73388
felipeam10 <felipeam10@hotmail.com>
2023-11-19 18:26:06 -0300

    v3.20 - BCrypt password encoder

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,3 +14,26 @@ public class DemoApplication {
 	}
 
 }
+
+/*
+Validação interna que faz o spring security
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+
+		boolean result = passwordEncoder.matches("123456", "$2a$10$MFoRQ5drewjKTRAEflSxuOgd/95djUtwCPJGsH6pWpfr6NeAxwZ/6");
+		System.out.println("RESULTADO = " + result);
+	}
+}
+*/
+
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$MFoRQ5drewjKTRAEflSxuOgd/95djUtwCPJGsH6pWpfr6NeAxwZ/6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$MFoRQ5drewjKTRAEflSxuOgd/95djUtwCPJGsH6pWpfr6NeAxwZ/6');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
