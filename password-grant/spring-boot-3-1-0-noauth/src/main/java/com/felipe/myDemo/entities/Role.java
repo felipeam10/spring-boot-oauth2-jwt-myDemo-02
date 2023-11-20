27fddb6fcabb3f3cd6a4de0b45a80983f095a2c9
felipeam10 <felipeam10@hotmail.com>
2023-11-20 09:53:44 -0300

    v3.21 -  checklist Spring Security PART 01

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/felipe/myDemo/entities/Role.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/felipe/myDemo/entities/Role.java
@@ -1,12 +1,13 @@
 package com.felipe.myDemo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +27,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
